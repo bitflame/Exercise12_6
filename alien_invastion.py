8631b300f02da8ed2c78325d434917dd208f400e
bitflame <monazzam@protonmail.com>
2024-05-23 16:15:01 -0400

    These latest mdifications do everything exercise 13-6 asked for

--- alien_invastion.py
+++ alien_invastion.py
@@ -1,9 +1,11 @@
 import sys
+from time import sleep
 
 import pygame
 
 from settings import Settings
 from ship import Ship
+from game_stats import GameStats
 from bullet import Bullet
 from alien import Alien
 
@@ -19,13 +21,31 @@ class AlienInvasion:
         # self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
         # self.settings.screen_width = self.screen.get_rect().width
         # self.settings.screen_height = self.screen.get_rect().height
-        pygame.display.set_caption("Alien Invasion")
+        pygame.display.set_caption("Sideways Shooter")
+        self.stats = GameStats(self)
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
         self._create_fleet()
         # set background color 
         self.bg_color = (230, 230, 230)
+        self.game_active = True
+        
+    def _ship_hit(self):
+        """Respond to the ship being hit by an alien."""
+        if self.stats.ships_left > 0:
+            # decrement ships_left
+            self.stats.ships_left -= 1
+            # Get rid of any remaining bullets and aliens
+            self.bullets.empty()
+            self.aliens.empty()
+            # Create a new fleet and center the ship 
+            self._create_fleet()
+            self.ship.center_ship()
+            # Pause
+            sleep(0.5)  
+        else: 
+            self.game_active = False  
         
     def _create_fleet(self):
         """Create the fleet of aliens"""
@@ -68,9 +88,10 @@ class AlienInvasion:
         """Start the main loop fo the game"""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            if self.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
     
@@ -98,7 +119,20 @@ class AlienInvasion:
         """Update the positions of all alisens in the fleet"""
         self._check_fleet_edges()
         self.aliens.update()
+        # Look for alien-ship collisions
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            self._ship_hit()
+        # Look for aliens hitting the left wall
+        self._check_aliens_bottom()
         
+    def _check_aliens_bottom(self):
+        """This is actually the left wall not the bottom"""
+        for alien   in self.aliens.sprites():
+            if alien.rect.left <= 0:
+                # Treat this the same as if the ship got hit. 
+                self._ship_hit()
+                break
+            
     def _update_bullets(self):
         """Update postion of bullets adn get rigd fo old bullets"""
         #update bullet position
@@ -108,6 +142,11 @@ class AlienInvasion:
             if bullet.rect.right >= self.screen.get_rect().right:
                 self.bullets.remove(bullet)         
         # print(len(self.bullets)) <- This is to make sure bullets are deleted.
+        self._check_bullet_alien_collisions()
+            
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions."""
+        # Remove any bullets and alients that have collided.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
         if not self.aliens:
             # Destroy existing bullets and create new fleet
--- readme.txt
+++ readme.txt
@@ -1,1 +1,7 @@
 This is another versison of allient invasion game that does what is asked in exercise 12-6
+
+Future ideas:
+Modify the game so when the shipps hit, there is an explosion and then the game is reset. The display
+logic can go right on top of _ship_hit() in alien_invasion file. 
+Add the game sound and the sound to the explosion, sound of lazer when bullets are fired, a different 
+explosion and sound when a bullet hits the alien ships 
